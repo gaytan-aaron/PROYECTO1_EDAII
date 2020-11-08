91f8ae20a5c1462c01ed19e761aaa752ae0c3b51
MarkAnthoner <54249176+MarkAnthoner@users.noreply.github.com>
2020-11-08 13:41:06 -0600

    Algoritmo de mezcla equilibrada que muestra iteraciones
    Se actualizó el algoritmo de mezcla equilibrada para poder mostrar iteraciones y las sublistas auxiliares.

--- Proyect/src/Mezcla/Mezcla.java
+++ Proyect/src/Mezcla/Mezcla.java
@@ -1,102 +1,213 @@
 package Mezcla;
 
 import java.util.*;
-import java.io.*;		//version de mezcla equilibrada para cadenas, aun no se manejan archivos
 
 public class Mezcla{
 
 	
-	public void mezclaEquilibrada(List<String> arr){	//lista original
+	public void mezclaEquilibrada(List<String> arr){
 
-		int left = 0;
-		int izq = 0; 
-		int right = arr.size() -1;
-		int der = right;
+		int inicio = 0;
+		int fin = 1;
+		int l = 1;
 
-		boolean ordenado = false;	//bandera que dirá si ya se ordenó la lista original
+		int k = 0;
+		int m = 0; 
+		int h = 0;
+		int cont = 0;
+
+		int inicioAux = 0;
+		int finAux = 0;
+
+		//StringBuilder temp = new StringBuilder();
+
+		boolean ordenado = false;
+		boolean bandera = true;		//este booleano hará que se ingresen elementos a la lista 1...
+									//... y luego a la 2 de forma intercalada.
+		List<String> arr1 = new LinkedList<>();
+		List<String> arr2 = new LinkedList<>();
+
+		int tamanoListaOriginal = arr.size();
+
 		do{
-			left = 0;
-			ordenado = true;
+
 			
-			while(left < right){
-				izq = left;
-				while((izq < right) && (arr.get(izq).compareTo(arr.get(izq+1)) <= 0)){
-					izq++;
+			bandera = true;
+
+			cont++;
+			System.out.println(" ");
+			System.out.println(cont + "a iteracion de mezcla natural");
+			
+		
+			//este proceso se encargará de ver qué elementos estan ordenados de forma natural y...
+			//...los ingresara de forma intercalada en cada lista auxiliar.
+
+			while( arr.size() != 0){	//mientras la lita original no este vacia.
+				inicio = 0;		//se inicializan siempre, porque al eliminar datos...
+								//... de la lista, todos los elementos se recorren a la izquierda.
+				fin = 1;
+				l = 1;
+				if(arr.size() > 1){	//si hay mas de un elemento
+					while( (arr.size() > l) && ((arr.get(l-1).compareTo(arr.get(l))) <= 0)){	//compara al elemento...
+																						//...actual con el anterior...
+																					//...siempre y cuando nos encontremos...
+																				//...en indices validos.
+						fin+=1;	// fin al final dirá el numero de elementos que entreran en la lista aux 1 o 2.
+						l+=1;
+
+					}
+					if(fin == tamanoListaOriginal){	//comprueba si ya no se puede crear un bloque de mayor tamaño,...
+													//...lo cual indicaría el final del ordenamiento
+							ordenado = true;
+							break;
+					}
 				}
-				der = izq + 1;
-				while((der == right -1) || (der < right) && (arr.get(der).compareTo(arr.get(der+1)) <= 0)){
-					der++;
+
+
+				
+				if(bandera){	//la bandera dira a que lista imprimirse.
+					if(arr.size() == 1){	//
+						arr1.add(arr.remove(0));	//si solo se detectó un elemento en orden, ...
+						inicio++;							//... se agrega a la lista aux y se elimina de la original.
+					}else{
+						while((inicio < fin) && (arr.size()>=1)){	//esto permitira que se recorran los elementos...
+																//...de forma natural y...
+																//...se ingresen a la lista aux. 
+							arr1.add(arr.remove(0));	//se estrae el primer elemento de la lista original...
+														//...pues siempre se recorren a la izquierda.
+							inicio++;
+						}
+					}
 				}
-				if(der<=right){
-					mezclaDirecta(arr);
-					ordenado = false;
+
+
+				
+				if(!bandera){
+					if(arr.size()==1){
+							arr2.add(arr.remove(0));
+							inicio++;
+					}else{
+						while((inicio < fin) && (arr.size()>=1)){
+							arr2.add(arr.remove(0));
+							inicio+=1;
+						}
+					}
 				}
-				left = izq; 
+		 
+				bandera = !bandera;		//esta bandera se intercalara para ingresar...
+										//...los elementos ya sea en la lista aux 1 o 2...
+										//...en cada iteracion
+				
+			}
+			
+			//se imprimirán los elementos de las listas auxiliares
+
+			System.out.println(" ");
+			System.out.println("Primer lista auxilixar");
+			System.out.println(" ");
+
+			imprimir(arr1);
+
+			
+			System.out.println(" ");
+			System.out.println("Segunda lista auxiliar");
+			System.out.println(" ");
+
+			imprimir(arr2);
+
+			//ahora se mezclaran las particiones y se ordenaran en la lista original...
+			//...por bloques
+
+			inicio = 0;
+			
+			while( (arr1.size() != 0) | (arr2.size() != 0) ){	//mientras una lista u otra no esté vacía
+
+				if(ordenado == true){	
+					break;		//si ya está ordenada la lista, se termina la ejecución de esta parte
+				}
+
+				k = 1;
+				m = 1;
+				
+				inicioAux = inicio;
+
+				while( (arr1.size() > k) && (arr1.get(k-1).compareTo(arr1.get(k)) <= 0 ) ){
+					k++;	//contará el numero de elementos que estan ordenados en orden natural...
+							//...en la lista aux 1.
+				}
+
+				while( (arr2.size() > m) && (arr2.get(m-1).compareTo(arr2.get(m)) <= 0 ) ){
+					m++;	//contará el numero de elementos que estan ordenados en orden natural...
+							//...en la lista aux 2.
+				}
+
+
+
+				if(arr1.size() != 0){
+					if(k == 1){	//
+						arr.add(arr1.remove(0));	//si solo se detectó un elemento en orden, ...
+						inicio++;							//... se agrega a la lista original y se elimina de la aux.
+					}else{
+						while((inicio < (k + inicioAux)) && (arr1.size()>=1)){	//esto permitira que se recorran los elementos...
+																//...de forma natural y...
+																//...se ingresen a la lista original. 
+							arr.add(arr1.remove(0));	//se estrae el primer elemento de la lista aux...
+														//...pues siempre se recorren los elementos a la izquierda.
+							inicio++;
+						}
+					}
+				}
+
+
+
+				if(arr2.size() != 0){
+					if(m==1){
+							arr.add(arr2.remove(0));
+							inicio++;
+					}else{
+						while((inicio < (m + k + inicioAux)) && (arr2.size()>=1)){	//se toma en cuenta el inicioAux en cada iteracion...
+															//...y así siempre se tendrá un indice de las particiones
+							arr.add(arr2.remove(0));
+							inicio+=1;
+						}
+					}
+				}
+
+				finAux = arr.size();
+
+					//ordenamiento de cada particion
+
+				//este metodo se encarga de ordenar cada particion dentro de la lista original, después de haber...
+				//...unido las particiones de cada lista auxiliar
+
+				for(int i = inicioAux + 1; i < arr.size(); i++){	
+					StringBuilder temp = new StringBuilder(arr.get(i));	//de string a stringbuilder. Se usó...
+									//...stringbuilder por su mutabilidad ya que fue necesaria esta característica...
+								//en esta seccion de una cadena auxiliar que cambia a lo largo de la ejecución
+					h = i - 1;
+					while((h>=(inicioAux)) && (arr.get(h).compareTo(temp.toString()) >= 0)){	//stringbuilder a string
+						arr.set(h+1,arr.get(h));
+						h--;
+					}
+					arr.set(h+1,temp.toString());	//stringbuilder a string
+				}
+
+
 			}
 
-		}while(!ordenado);
+			System.out.println("Lista original: ");
+			System.out.println(" ");
+			imprimir(arr);
 
+
+
+		}while(!ordenado);	//si ya está ordenada la lista, se termina el programa
 	}
 
 
 
 
-
-
-
-	public void mezclaDirecta(List<String> arr){
-		int i;
-		int j;
-		int k;
-		if (arr.size() > 1){	//si aunque sea hay dos elementos se ejecuta
-			int elementosIzquierda = arr.size() / 2;
-			int elementosDerecha = arr.size() - elementosIzquierda;
-			List<String> arr1 = new LinkedList<>();	//dos estructuras auxiliares
-			List<String> arr2 = new LinkedList<>();
-
-			for(i = 0; i < elementosIzquierda; i++){	//se pasa toda la primera mitad de la lista a una lista auxiliar
-				arr1.add(i, arr.get(i));
-			}
-
-			for(i = elementosIzquierda; i < elementosIzquierda + elementosDerecha; i++){	//se pasa la segunda mitad de la lista a la otra lista auxiliar
-				arr2.add(i-elementosIzquierda,arr.get(i)); 
-			}
-
-			mezclaDirecta(arr1);	
-			mezclaDirecta(arr2);
-			
-			i = 0;
-			j = 0;
-			k = 0;
-			while(arr1.size() != j && arr2.size() != k){
-				if(	arr1.get(j).compareTo(arr2.get(k)) < 0){	//de esta manera, es primero lexicograficamente arr1.get(j) o menor
-					arr.set(i,arr1.get(j)); 
-					i++;
-					j++;
-				}else{
-					arr.set(i,arr2.get(k));
-					i++;
-					k++;
-				}
-			}
-			while(arr1.size() != j){
-				arr.set(i,arr1.get(j));
-				i++;
-				j++;
-			}
-			while(arr2.size() != k){
-				arr.set(i, arr2.get(k));
-				i++;
-				k++;
-			}
-		}
-
-		//return arr;
-	}
-
-
-
-public void imprimir(List<String> listaDeImpresion){
+public static void imprimir(List<String> listaDeImpresion){
 
 	for(String recorrido:listaDeImpresion){
 		System.out.println(recorrido);
--- Proyect/src/Mezcla/Prueba.java
+++ Proyect/src/Mezcla/Prueba.java
@@ -3,6 +3,7 @@ package Mezcla;
 import java.util.*;
 import Mezcla.Mezcla;
 
+
 public class Prueba{
 
 	public static void main(String[] args){
@@ -10,28 +11,37 @@ public class Prueba{
 
 		List<String> lista = new LinkedList<>();
 
-		lista.add(0, "Probando una cadena larga");
+		
 
-		lista.add("Hola");
+		lista.add("Famigo");
+		lista.add("Zetas");
+
 		lista.add("Como");
 		lista.add("Estas");
-		lista.add("Amigo");
+		lista.add("Fernando");
+
 		lista.add("Cadena");
 		lista.add("Marco");
+
+		lista.add("Antonio");
 		lista.add("Sanchez");
+
 		lista.add("Antonio");
 		lista.add("Emanuel");
+		lista.add("Run to you");
+		lista.add("Yes");
 		lista.add("Zoom");
+
 		lista.add("Borja");
 		lista.add("Garcia");
 		lista.add("Marquez");
-		lista.add("Ruben");
-		lista.add("Dario");
+		lista.add("Cuben");
+		//lista.add("Dario");
 
 		Mezcla prueba = new Mezcla();
 		prueba.mezclaEquilibrada(lista);
 
-		prueba.imprimir(lista);
+		//prueba.imprimir(lista);
 
 
 	}
